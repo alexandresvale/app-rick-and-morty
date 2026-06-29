a68d65c8dcf995b0fe77c839422be95d47bb3d0a
alexandresvale <alexandre.vale@live.com>
2026-06-28 23:34:57 -0300

    Ajustado gráfico de navegação

--- feature/characters/impl/src/main/java/com/alexandresvale/rickandmorty/feature/characters/impl/navigation/CharactersNavigation.kt
+++ feature/characters/impl/src/main/java/com/alexandresvale/rickandmorty/feature/characters/impl/navigation/CharactersNavigation.kt
@@ -1,14 +1,35 @@
 package com.alexandresvale.rickandmorty.feature.characters.impl.navigation
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.alexandresvale.rickandmorty.feature.characters.impl.presentation.CharacterDetailsScreen
+import com.alexandresvale.rickandmorty.feature.characters.impl.presentation.CharacterDetailsViewModel
 import com.alexandresvale.rickandmorty.feature.characters.impl.presentation.CharactersScreen
 import com.alexandresvale.rickandmorty.feature.characters.impl.presentation.CharactersViewModel
 import org.koin.androidx.compose.koinViewModel
 
-fun NavGraphBuilder.charactersScreen() {
+fun NavGraphBuilder.charactersGraph(
+    onNavigateToDetails: (Int) -> Unit,
+    onNavigateBack: () -> Unit
+) {
     composable("characters_list") {
         val viewModel: CharactersViewModel = koinViewModel()
-        CharactersScreen(viewModel = viewModel)
+        CharactersScreen(
+            viewModel = viewModel,
+            onCharacterClick = onNavigateToDetails
+        )
+    }
+
+    composable(
+        route = "character_details/{characterId}",
+        arguments = listOf(navArgument("characterId") { type = NavType.StringType })
+    ) {
+        val viewModel: CharacterDetailsViewModel = koinViewModel()
+        CharacterDetailsScreen(
+            viewModel = viewModel,
+            onBackClick = onNavigateBack
+        )
     }
 }
